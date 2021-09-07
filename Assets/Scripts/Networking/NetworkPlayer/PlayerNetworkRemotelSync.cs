b2c9e34cfadeac74a877ced978e62126c0a601e8
Umair <umairwasim001@gmail.com>
2021-09-07 14:16:22 +0500

    NetworkLocal and Remote Player

---
+++ Assets/Scripts/Networking/NetworkPlayer/PlayerNetworkRemotelSync.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetworkRemotelSync : MonoBehaviour
+{
+    //Reference relevent network data about a player who is remotelty connected.
+    //stores MatchId and user attributes of the remote 
+    public RemotePlayerNetworkData networkData;
+}
