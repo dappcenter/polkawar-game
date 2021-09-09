044c6a99b84eae57fa59d358db1c9e0d20b40f7d
Imran Bin Azhar <imran.zaidiedu@gmail.com>
2021-09-10 03:59:25 +0500

    Dice progress

--- Assets/Scripts/Game/DicesController.cs
+++ Assets/Scripts/Game/DicesController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DicesController : MonoBehaviour
+public class DicesController : SingletonMB<DicesController>
 {
     [System.Serializable]
     public class PosRot
@@ -11,19 +11,50 @@ public class DicesController : MonoBehaviour
         public Vector3 rotation;
     }
 
+    public Transform fourDice, twentyDice;
+
     public SerializableDictionary<int, Vector3> twentySidedDice = new SerializableDictionary<int, Vector3>();
 
     public SerializableDictionary<int, List<PosRot>> tenSidedDice = new SerializableDictionary<int, List<PosRot>>();
 
-    // Start is called before the first frame update
-    void Start()
+    public void GiveItARoll(System.Action onComplete)
     {
-        
+        StartCoroutine(RollTheDices(onComplete));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RollTheDices(System.Action onComplete)
     {
-        
+        yield return null;
+
+        List<Vector3> twentySideRotations = new List<Vector3>();
+
+        foreach (var item in twentySidedDice)
+        {
+            twentySideRotations.Add(item.Value);
+        }
+
+        List<PosRot> allPosRotFourDice = new List<PosRot>();
+
+        foreach (var item in tenSidedDice)
+        {
+            allPosRotFourDice.AddRange(item.Value);
+        }
+
+        for (int i = 0; i < Random.Range(7, 12); i++)
+        {
+            int fourRand = Random.Range(0, allPosRotFourDice.Count);
+            int twentyRand = Random.Range(0, twentySideRotations.Count);
+
+            fourDice.position = allPosRotFourDice[fourRand].position;
+            fourDice.rotation = Quaternion.Euler(allPosRotFourDice[fourRand].rotation);
+
+            twentyDice.rotation = Quaternion.Euler(twentySideRotations[twentyRand]);
+
+            yield return new WaitForSeconds(0.075f);
+        }
+
+        yield return null;
+
+        onComplete?.Invoke();
     }
 }
