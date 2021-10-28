fd9655bbb3b9bbda19ac2c690d20267d36fe2715
Umair <umairwasim001@gmail.com>
2021-10-28 20:34:01 +0500

    PlayerCombatBehaviour script added with damage and attackPoint

---
+++ Assets/Scripts/Player/PlayerCombatBehaviour.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatBehaviour : MonoBehaviour
+{
+    public int damage = 10;
+    public Transform attackPoint; 
+}
